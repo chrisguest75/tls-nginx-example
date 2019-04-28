a3dd7eccaced81b4a3870d10f5b2323779a18c01
chrisguest75 <chris.guest75@hotmail.com>
2019-04-28 15:48:19 +0100

    Intermediate commit that links up the mirror to the DB. It writes out the mirror variables to the DB on each request. Also added linting with flake8.

--- py-request-mirror/mirror.py
+++ py-request-mirror/mirror.py
@@ -2,7 +2,8 @@
 from flask import Flask, request, abort, jsonify
 import os
 import write_request
-db_writer = write_request.write_request("mysql+mysqlconnector://root:example@127.0.0.1:3306/requests_tracking")
+
+db_writer = None
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST', 'PUT', 'DELETE'])
@@ -27,5 +28,27 @@ if __name__ == "__main__":
     if server_port is None:
         print(f"PORT environment variable not set")
         exit(1)
+
+    dbuser = os.environ.get('DBUSER')
+    if dbuser is None:
+        print(f"DBUSER environment variable not set")
+        exit(1)
+
+    dbpassword = os.environ.get('DBPASSWORD')
+    if dbpassword is None:
+        print(f"DBPASSWORD environment variable not set")
+        exit(1)
+
+    dbhost = os.environ.get('DBHOST')
+    if dbhost is None:
+        print(f"DBHOST environment variable not set")
+        exit(1)
+
+    dbport = os.environ.get('DBPORT')
+    if dbport is None:
+        print(f"DBPORT environment variable not set")
+        exit(1)
+
+    db_writer = write_request.write_request(f"mysql+mysqlconnector://{dbuser}:{dbpassword}@{dbhost}:{dbport}/requests_tracking")
 
     app.run(port=server_port, host='0.0.0.0')
